d54a0d203ae5405aaf62fa7ed800114900999a34
latifniz <abdullatifnizamani517@gmail.com>
2025-03-05 00:31:40 +0500

    added prod env

--- next.config.js
+++ next.config.js
@@ -1,18 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental:{
-        serverComponentsExternalPackages: [
-            '@react-email/render',
-        ],
-        // images:{
-        //     remotePatterns:[
-        //         {
-        //             protocol: 'https',
-        //             hostname: 'cdn.jsdelivr.net',
-        //         }
-        //     ]
-        // }
-    }
-}
+  experimental: {
+    serverComponentsExternalPackages: ["@react-email/render"],
+    // images: {
+    //   remotePatterns: [
+    //     {
+    //       protocol: 'https',
+    //       hostname: 'cdn.jsdelivr.net',
+    //     }
+    //   ]
+    // }
+  },
+  // Add server configuration to use the port from the environment variable
+  server: {
+    port: process.env.PORT || 3000, // Default to port 3000 if PORT is not set
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
